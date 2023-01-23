3f814a27b2d3362b7aaf43b8999074fc5c365bc8
vaibhavcrane <vaibhavcrane@me.com>
2023-01-23 02:45:24 -0500

    added navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import "./styles/main.scss";
 
 import AppProvider from "./providers/providers";
+import { Navbar } from "./components";
 
 function App() {
 	//TODO: Add theme toggle
 	return (
 		<div className='App dark'>
+			<Navbar />
 			<AppProvider />
 		</div>
 	);
---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,26 @@
+import "../../styles/main.scss";
+
+export const Navbar = () => {
+	return (
+		<nav className='navbar'>
+			<div className='navbar__inner'>
+				<div className='navbar__logo'>
+					<a href='/'>f1 app</a>
+				</div>
+				<div className='navbar__menu'>
+					<ul className='navbar__menu-list'>
+						<li className='navbar__menu-list-item'>
+							<a href='/'>Current Season</a>
+						</li>
+						<li className='navbar__menu-list-item'>
+							<a href='/archives'>Archives</a>
+						</li>
+						<li className='navbar__menu-list-item'>
+							<a href='/about'>About</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+	);
+};
