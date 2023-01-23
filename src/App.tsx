c487df1c7c78e2382dcd4c1314b234758c9ec96a
vaibhavcrane <vaibhavcrane@me.com>
2023-01-22 22:30:05 -0500

    using AppProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import "./styles/main.scss";
 
+import AppProvider from "./providers/providers";
+
 function App() {
 	//TODO: Add theme toggle
-	return <div className='App dark'>Hello</div>;
+	return (
+		<div className='App dark'>
+			<AppProvider />
+		</div>
+	);
 }
 
 export default App;
