1a8d9702d80eecf69b8d73a752e3d064ba099521
vaibhavcrane <vaibhavcrane@me.com>
2023-01-24 02:48:32 -0500

    defined types

--- src/components/Grid/index.tsx
+++ src/components/Grid/index.tsx
@@ -1,9 +1,12 @@
-import { SmallGridProps } from "../../types";
+type GridProps = {
+	children: React.ReactNode;
+	className?: string;
+};
 
-export const MainGrid = ({ style, className, children }: any) => {
+export const MainGrid = ({ children, className }: GridProps) => {
 	return <div className={`grid ${className}`}>{children}</div>;
 };
 
-export const SmallGrid = ({ children, className = "" }: SmallGridProps) => {
+export const SmallGrid = ({ children, className = "" }: GridProps) => {
 	return <div className={`${className}`}>{children}</div>;
 };
