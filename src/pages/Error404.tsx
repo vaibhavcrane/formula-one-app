5f2830ef1ed37806e0db69dc0d803769ba8c1e2c
vaibhavcrane <vaibhavcrane@me.com>
2023-01-25 00:51:56 -0500

    404 page

---
+++ src/pages/Error404.tsx
@@ -0,0 +1,13 @@
+export const Error404 = () => {
+	return (
+		<div className='container'>
+			<div className='error-404'>
+				<p>We could not find the page, but who's fault is it really?</p>
+				<img
+					src='src/assets/404/meme.jpeg'
+					alt='404 meme'
+				/>
+			</div>
+		</div>
+	);
+};
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,10 +1,15 @@
 import { Routes, Route } from "react-router-dom";
 import { CurrentSeason } from "../pages/CurrentSeason";
+import { Error404 } from "../pages/Error404";
 
 export default function AppRouter() {
 	return (
 		<>
 			<Routes>
+				<Route
+					path='*'
+					element={<Error404 />}
+				/>
 				<Route
 					path='/'
 					element={<CurrentSeason />}
