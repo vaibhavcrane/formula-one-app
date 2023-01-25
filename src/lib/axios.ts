b00cad526b76b9cb9b84fb22f7c3e1c8ce45ff5e
vaibhavcrane <vaibhavcrane@me.com>
2023-01-24 22:54:45 -0500

    fix for unsecure requests

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 /* Creating a new axios instance with a baseURL property. */
-const ergast = axios.create({ baseURL: "http://ergast.com/api/f1/" });
+const ergast = axios.create({ baseURL: "https://ergast.com/api/f1/" });
 
 export default ergast;
